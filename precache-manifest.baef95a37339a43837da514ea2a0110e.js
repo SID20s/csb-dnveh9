078f2198ee97183d7c417766f6fc1f8d8eebd587
Code Sandbox <hello@codesandbox.io>
2022-03-25 06:46:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.baef95a37339a43837da514ea2a0110e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94af6734d31ab5164b14cfeec58cf4d3",
+    "url": "https://sid20s.github.io/csb-dnveh9/index.html"
+  },
+  {
+    "revision": "391fdbc234f8dd09fa1a",
+    "url": "https://sid20s.github.io/csb-dnveh9/static/css/main.721f9b8f.chunk.css"
+  },
+  {
+    "revision": "530d1a7b462acf80ca0e",
+    "url": "https://sid20s.github.io/csb-dnveh9/static/js/2.fea30b6c.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "https://sid20s.github.io/csb-dnveh9/static/js/2.fea30b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391fdbc234f8dd09fa1a",
+    "url": "https://sid20s.github.io/csb-dnveh9/static/js/main.f4482d5a.chunk.js"
+  },
+  {
+    "revision": "d18b34d82745dcd7eeab",
+    "url": "https://sid20s.github.io/csb-dnveh9/static/js/runtime-main.128345d4.js"
+  },
+  {
+    "revision": "d01dd44df39e4af93aac2b90a60ad242",
+    "url": "https://sid20s.github.io/csb-dnveh9/static/media/Covid19Tracker.d01dd44d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://sid20s.github.io/csb-dnveh9/precache-manifest.baef95a37339a43837da514ea2a0110e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://sid20s.github.io/csb-dnveh9/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
